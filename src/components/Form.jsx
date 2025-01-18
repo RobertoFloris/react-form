1ff2f289d865554cfe5a189ce4bdc02c45150265
RobertoFloris <florisroberto99@gmail.com>
2025-01-18 16:24:15 +0100

    [add] form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react"
+
 const Form = () => {
+
+  const [title, setTitle] = useState("")
+
+  const handlerSubmit = (e) => {
+    setTitle(e.target.value);
+  };
+
   return (
-    <div>Form</div>
+    <form className="container mt-5">
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="title"
+          value={title}
+          onChange={handlerSubmit} />
+      </div>
+
+
+      <button type="submit" className="btn btn-primary">Invia</button>
+    </form>
   )
 }
 

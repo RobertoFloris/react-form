56273cc064f8582a3504afe5b4234d8106dd1e72
RobertoFloris <florisroberto99@gmail.com>
2025-01-20 16:47:14 +0100

    [add] possibility of editing in form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,11 +3,22 @@ import { useState } from "react";
 const Form = () => {
   const [title, setTitle] = useState("");
   const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
 
   const handlerSubmit = (e) => {
     e.preventDefault();
     if (title.trim()) {
-      setList([...list, title]);
+      if (isEditing) {
+        const updatedList = list.map((item, i) =>
+          i === editIndex ? title : item
+        )
+        setList(updatedList);
+        setIsEditing(false);
+        setEditIndex(null);
+      } else {
+        setList([...list, title]);
+      }
       setTitle("");
     }
   };
@@ -15,11 +26,17 @@ const Form = () => {
   const handlerDelete = (index) => {
     const newList = list.filter((item, i) => i !== index);
     setList(newList);
-  }
+  };
+
+  const handlerEdit = (index) => {
+    setTitle(list[index]);
+    setIsEditing(true);
+    setEditIndex(index);
+  };
 
   return (
     <form className="container mt-5" onSubmit={handlerSubmit}>
-      <div className="mb-3">
+      <div>
         <label className="form-label">Title</label>
         <input
           type="text"
@@ -30,16 +47,25 @@ const Form = () => {
         />
       </div>
 
-      <ul>
+      <button type="submit" className="btn btn-primary m-3">
+        {isEditing ? "Modifica" : "Invia"}
+      </button>
+
+      <ul className="list-group">
         {list.map((item, index) => (
-          <li key={index}>
-            {item}
-            <span className="m-5" onClick={() => handlerDelete(index)}>×</span>
+
+          <li key={index} className="list-group-item d-flex justify-content-between">
+            <span>{item}</span>
+            <div>
+              <span className="delete m-3" onClick={() => handlerDelete(index)}>❌</span>
+              <span className="edit m-3" onClick={() => handlerEdit(index)}>✏️</span>
+            </div>
           </li>
         ))}
       </ul>
 
-      <button type="submit" className="btn btn-primary">Invia</button>
+
+
     </form>
   );
 };
